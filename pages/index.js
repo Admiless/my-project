a3a83825e88a3d5478188446088b196437d51bdf
Admiless <admiless@me.com>
2025-02-03 20:58:10 +0200

    Delete pages/index.js

--- pages/index.js
+++
@@ -1,8 +0,0 @@
-import ProductForm from "@/components/ProductForm"; // ✅ Без пробела, верный регистр
-export default function Home() {
-  return (
-    <div className="flex justify-center items-center min-h-screen bg-gray-100">
-      <ProductForm />
-    </div>
-  );
-}
