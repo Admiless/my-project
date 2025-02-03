9bf14f5de3d9c01a0d282d7d7838b42f1e404e9e
Admiless <admiless@me.com>
2025-02-03 18:29:28 +0200

    oi

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
-iimport ProductForm from "@/components/ProductForm";
+import ProductForm from "@/components/ProductForm";
+
 export default function Home() {
   return (
     <div className="flex justify-center items-center min-h-screen bg-gray-100">
