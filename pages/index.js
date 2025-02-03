e5bed464926aaf8a14c65d3c163aa9438d70c6e8
Admiless <admiless@me.com>
2025-02-03 20:59:25 +0200

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import ProductForm from "@/components/ProductForm";
+
+export default function Home() {
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-gray-100">
+      <ProductForm />
+    </div>
+  );
+}
